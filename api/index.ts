e109f4128253ec9439ccbc425c7d78796e609fc3
i-atanasov <ivayloatanasov@zohomail.eu>
2025-08-07 12:34:37 +0300

    types/cors

--- api/index.ts
+++ api/index.ts
@@ -5,8 +5,8 @@ import { signUpHandler } from '../handlers/signUpHandler';
 
 const app = express();
 
-app.use(cors({origin: ["http://localhost:3000", "https://osogovorun.vercel.app/"]})); 
-app.use(express.json()); // Middleware to parse JSON bodies
+app.use(cors({origin: ["http://localhost:3000", "https://osogovorun.vercel.app/", "https://osogovo.run"]})); // Allow CORS from specific origins
+app.use(express.json()); 
 
 app.get("/test-cors-get", function (req, res) {
   res.status(200).json({ success: true });
@@ -16,10 +16,10 @@ app.post("/test-cors-post", function (req, res) {
   res.status(200).json({ success: true });
 });
 
-
 app.get('/', (req, res) => {
-    res.send('Welcome to the Osogovo Run API');
+    res.status(200).send('Welcome to the Osogovo Run API');
 });
+
 app.post('/register', signUpHandler.post);
 
 // Start the server
